b2600619eed9ebb626f0506421e6bdddae7fa1ed
Adam Osiecki <osiecki.adam@outlook.com>
2019-11-25 20:32:13 +0100

    Job monitoring added

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
 
 let callback = null;
 if (process.argv[2]) {
-  console.log(process.argv[2]);
   switch (process.argv[2]) {
     case DELETE_APEX_LOGS:
       callback = deleteApexLogs;
@@ -49,8 +48,6 @@ function login(callback) {
 }
 
 function deleteApexLogs() {
-  conn.bulk.pollInterval = 10000;
-  conn.bulk.pollTimeout = 60000;
   conn
     .sobject("ApexLog")
     .find({}, ["Id"])
@@ -59,12 +56,43 @@ function deleteApexLogs() {
       if (err) {
         return console.error(err);
       }
-      console.log(records.length);
-    })
-    .del({ allowRecursive: true }, function(err, rets) {
-      if (err) {
-        return console.error(err);
-      }
-      log(chalk.bold.green("NUMBER OF DELETED RECORDS: ") + rets.length);
+      console.log("Number of ApexLog records to be deleted: " + records.length);
+      let job = conn.bulk.createJob("ApexLog", "delete");
+      let batch = job.createBatch();
+      batch.execute(records);
+      batch.on("queue", batchInfo => {
+        batch.poll(1000, 10000000);
+        console.log("Delete job is started. Id of the job: " + batchInfo.jobId);
+      });
+      batch.on("progress", response => {
+        batch.check().then(batchInfo => {
+          process.stdout.write(
+            "\x1Bc\rProcessed records: " +
+              batchInfo.numberRecordsProcessed +
+              " / " +
+              records.length
+          );
+        });
+      });
+      batch.on("response", response => {
+        batch.check().then(batchDetails => {
+          console.log(
+            "\nJob is finished, status of the job is:",
+            batchDetails.state,
+            ". Total processing time was " + batchDetails.totalProcessingTime,
+            "ms"
+          );
+          if (batchDetails.numberRecordsFailed !== "0") {
+            console.log(
+              "Number of failed records: " + batchDetails.numberRecordsFailed
+            );
+          } else {
+            console.log("All records were deleted sucessfully");
+          }
+        });
+      });
+      batch.on("error", response => {
+        console.log("response", response);
+      });
     });
 }
